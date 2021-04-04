62975f8500253aec6e5bc739f257abf1daf0daf8
Paavo Kemppainen <paavo.kemppainen@tuni.fi>
2021-04-04 14:29:43 +0300

    Password hashing

--- api/routes/user.js
+++ api/routes/user.js
@@ -1,8 +1,23 @@
 var express = require('express');
 var router = express.Router();
+var userBD = require('../database/userDB');
+var passwordHash  = require('password-hash');
 
 /* POST new user listing. */
 router.post('/', function(req, res, next) {
+  let body = req.body['user'];
+  console.log(req.body['user']);
+  const hash_salt = passwordHash.generate('testPassword');
+  console.log("HASH", hash_salt)
+  body['pw_hash_salt'] = hash_salt;
+  console.log(body)
+  let result = userBD.addUser(body);
+  console.log("Result: ", result)
+  res.send('new user added');
+});
+
+/* GET user listing. */
+router.get('/', function(req, res, next) {
   res.send('new user added');
 });
 
